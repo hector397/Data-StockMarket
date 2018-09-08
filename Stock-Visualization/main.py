83d79f971e156752fab23ce23d3d1c3d08b17b16
hector397 <39014994+hector397@users.noreply.github.com>
2018-09-08 11:49:25 +0200

    Delete main.py

--- Stock-Visualization/main.py
+++
@@ -1,7 +0,0 @@
-from alpha_vantage.timeseries import TimeSeries
-
-
-ts = TimeSeries(key='LH3WNUH2LPYLE6R9', output_format='pandas')
-
-data, meta_data = ts.get_intraday(symbol='TSLA',interval='1min', outputsize='compact')
-print(data.head())
