3a1b1b8acb623c5b4af2ec1ba057740652216b8c
hector397 <39014994+hector397@users.noreply.github.com>
2019-04-02 18:17:42 +0200

    Add files via upload

--- Stock-Visualization/main.py
+++ Stock-Visualization/main.py
@@ -1,10 +1,4 @@
-from alpha_vantage.timeseries import TimeSeries
-import matplotlib.pyplot as plt
+from DataRequester import DataRequester
 
-
-ts = TimeSeries(key='LH3WNUH2LPYLE6R9', output_format='pandas')
-
-data, meta_data = ts.get_intraday(symbol='TSLA',interval='1min', outputsize='full')
-data['4. close'].plot()
-plt.title('Intraday Times Series for the MSFT stock (1 min)')
-plt.show()
+dataRequester = DataRequester()
+print(dataRequester.getSymbols())
